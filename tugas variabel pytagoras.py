58e8b1ab4675fd4ece03fbc36124f280129e94ea
5520123007-laluwilanggenggeni <144973271+5520123007-laluwilanggenggeni@users.noreply.github.com>
2023-09-19 18:09:31 +0800

    Update tugas variabel pytagoras.py
    perbaikan rumus pytagoras

--- tugas variabel pytagoras.py
+++ tugas variabel pytagoras.py
@@ -1,7 +1,5 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> tinggi = 4
->>> alas = 3
->>> pytagoras = tinggi + alas # Rumus pythagoras salah 
->>> print(tinggi + alas)
-7
+alas=3
+tinggi=4
+pytagoras=1/2*alas*tinggi
+print(pytagoras)
+6.0
