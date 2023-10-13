70c91d8c73bf0f3ca8f55320b12b36dea39406b4
5520123007-laluwilanggenggeni <144973271+5520123007-laluwilanggenggeni@users.noreply.github.com>
2023-10-13 14:38:34 +0800

    Update tugas autentikasi.py

--- tugas autentikasi.py
+++ tugas autentikasi.py
@@ -3,7 +3,7 @@ password = input("masukkan password: ")
 code_username = 'lalu wilanggeng geni'
 code_password = 123456
 
-if username == code_username or password == code_password:
+if username == code_username and password == code_password:
     print("selamat datang",username)
 else:
     print("username atau password salah")
