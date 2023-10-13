92c9c60dd9f78a4cc5634b6bf927dd6023fdf848
5520123007-laluwilanggenggeni <144973271+5520123007-laluwilanggenggeni@users.noreply.github.com>
2023-10-13 11:03:29 +0800

    Add files via upload

---
+++ tugas autentikasi.py
@@ -0,0 +1,13 @@
+username = input('masukkan username: ')
+password = input("masukkan password: ")
+code_username = 'lalu wilanggeng geni'
+code_password = 123456
+
+if username == code_username or password == code_password:
+    print("selamat datang",username)
+else:
+    print("username atau password salah")
+
+
+
+
